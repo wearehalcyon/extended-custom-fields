8990e8696a5fad100b47352fd8d067cb5c0080a1
Roman Chernov <thewitness45@gmail.com>
2024-05-21 14:33:58 +0300

    init

---
+++ extended-custom-fields.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Extended Custom Fields
+Plugin URI: https://github.com/wearehalcyon/extended-custom-fields
+Description: This plugin provide extended (advanced) custom fields to your WordPress site. Plugin based on Carbon Fields.
+Author: Roman A
+Version: 0.0.5
+Author URI: mailto:thewitness45@gmail.com
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'Invalid request.' );
+}
+
